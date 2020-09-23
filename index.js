9d2aa3fb0f850c86e346b0353516dafe7cdecbef
Pankaj Negi <pnegi.dun@gmail.com>
2020-09-23 13:28:32 +0530

    added comments

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const redis = require('redis')
 const client = redis.createClient()
 
-
+//Will listen to all errors occure in client
 client.on("error", (err)=>{console.log(err)})
 
+//Setting the data using set
 client.set("name", "Pankaj", (err, message)=>{
     if(err) throw err
     console.log(message)
 })
  
-
+//Getting the values and printing using redis.print
 client.get("name", redis.print)
 
  
